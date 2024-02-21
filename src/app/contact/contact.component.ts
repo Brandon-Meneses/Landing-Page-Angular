c9929d75eff739856ea7342eeb387cf15afa730c
Brandon-Meneses <134113321+Brandon-Meneses@users.noreply.github.com>
2024-02-21 12:47:37 -0500

    Formularios reactivos, se usa constructor y formbuilder, para colocar mas de una reaccion se debe colocar en el ts en corchetes para obtener un array de reaction, muy importante

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
+  formularioContacto: FormGroup;
+  constructor(private form: FormBuilder) {
+    this.formularioContacto = this.form.group({
+      nombre: ['',[Validators.required, Validators.minLength(3)]],
+      email: ['',[Validators.required,Validators.email]]
+    })
+
+  }
+  hasErrors(controlName: string, errortype: string) {
+    return this.formularioContacto.get(controlName)?.hasError(errortype) && this.formularioContacto.get(controlName)?.touched;
+  }
+  
+  enviar() {
+    console.log(this.formularioContacto);
+  }
 
 }
